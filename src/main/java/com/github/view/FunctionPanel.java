8bd7706dc799168d5c4c465fe51aa4ad4b27ead9
pwlgbb <pgraczek@gmail.com>
2016-05-13 14:20:16 +0200

    Poprawki do formy i obliczanie

--- src/main/java/com/github/view/FunctionPanel.java
+++ src/main/java/com/github/view/FunctionPanel.java
@@ -1,15 +1,13 @@
 package com.github.view;
 
-import com.intellij.uiDesigner.core.GridConstraints;
-import com.intellij.uiDesigner.core.GridLayoutManager;
-import com.intellij.uiDesigner.core.Spacer;
+import com.github.algorithm.MyFunction;
+import com.github.algorithm.SimulatedAnnealing;
+import org.apache.commons.lang.ArrayUtils;
 
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.concurrent.ExecutionException;
 
 public class FunctionPanel {
     public JPanel panel;
@@ -22,71 +20,78 @@ public class FunctionPanel {
     private JLabel labRightLimit;
     private JLabel labFunction;
     private JLabel labResult;
+    private JLabel labStatus;
+    private JLabel labStatusResult;
+    private SimulatedAnnealing sa = new SimulatedAnnealing();
 
     public FunctionPanel() {
-        $$$setupUI$$$();
-
         btnCompute.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 super.mouseClicked(e);
-                //tu obliczamy
+
+                if (cbFunction.getSelectedIndex() > -1) {
+                    ComboboxItem item = (ComboboxItem) cbFunction.getSelectedItem();
+
+                    SwingWorker<Double[], MyFunction> worker = new SwingWorker<Double[], MyFunction>() {
+                        @Override
+                        protected Double[] doInBackground() throws Exception {
+                            labStatusResult.setText("Obliczanie...");
+
+                            double[] val = sa.simulatedAnnealing(
+                                    item.getFunction(),
+                                    n -> n * 0.999,
+                                    1000,
+                                    0.001,
+                                    2,
+                                    (Double) spLeftLimit.getValue(),
+                                    (Double) spRightLimit.getValue()
+                            );
+                            return ArrayUtils.toObject(val);
+                        }
+
+                        @Override
+                        protected void done() {
+                            try {
+                                double[] args = ArrayUtils.toPrimitive(get());
+
+                                Double res = item.getFunction().f(args);
+
+                                labStatusResult.setText(res.toString());
+
+                                StringBuilder sb = new StringBuilder();
+
+                                sb.append("<html>");
+
+                                for (int i = 0; i < args.length; i++) {
+                                    sb.append(String.format("x[%d] = %s<br>", i, args[i]));
+                                }
+
+                                sb.append("</html>");
+
+                                labStatusResult.setText(sb.toString());
+                                tfResult.setText(res.toString());
+                            } catch (InterruptedException | ExecutionException ex) {
+                                labStatusResult.setText("Błąd");
+                                tfResult.setText(ex.toString());
+                            }
+                        }
+                    };
+
+                    worker.run();
+                }
             }
         });
-        cbFunction.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                ComboboxItem item = (ComboboxItem) cbFunction.getSelectedItem();
 
-                spLeftLimit.setValue(item.getLeft());
-                spRightLimit.setValue(item.getRight());
-            }
+        cbFunction.addItemListener(e -> {
+            ComboboxItem item = (ComboboxItem) cbFunction.getSelectedItem();
+
+            spLeftLimit.setValue(item.getLeft());
+            spRightLimit.setValue(item.getRight());
         });
-    }
 
-    private void $$$setupUI$$$() {
-        panel = new JPanel();
-        panel.setLayout(new BorderLayout(0, 0));
-
-        final JPanel panel1 = new JPanel();
-        panel1.setLayout(new GridLayoutManager(6, 2, new Insets(0, 0, 0, 0), -1, -1));
-        panel.add(panel1, BorderLayout.CENTER);
-
-        spLeftLimit = new JSpinner();
-        panel1.add(spLeftLimit, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        labLeftLimit = new JLabel();
-        labLeftLimit.setText("Left Limit");
-        panel1.add(labLeftLimit, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-
-        final Spacer spacer1 = new Spacer();
-        panel1.add(spacer1, new GridConstraints(3, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_VERTICAL, 1, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
-
-        spRightLimit = new JSpinner();
-        panel1.add(spRightLimit, new GridConstraints(2, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        labRightLimit = new JLabel();
-        labRightLimit.setText("Right Limit");
-        panel1.add(labRightLimit, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-
-        panel1.add(cbFunction, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        labFunction = new JLabel();
-        labFunction.setText("Function");
-        panel1.add(labFunction, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-
-        final JPanel panel2 = new JPanel();
-        panel2.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-        panel1.add(panel2, new GridConstraints(3, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, null, null, 0, false));
-
-        btnCompute = new JButton();
-        btnCompute.setText("Oblicz");
-        panel1.add(btnCompute, new GridConstraints(5, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        tfResult = new JTextField();
-        tfResult.setText("");
-        panel1.add(tfResult, new GridConstraints(4, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(150, -1), null, 0, false));
-        labResult = new JLabel();
-        labResult.setText("Wynik");
-        panel1.add(labResult, new GridConstraints(4, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-
-        cbFunction = new JComboBox<>();
+        spLeftLimit.setModel(new SpinnerNumberModel(0, -10000, 10000, 0.01));
+        spRightLimit.setModel(new SpinnerNumberModel(0, -10000, 10000, 0.01));
     }
 
     public JComponent $$$getRootComponent$$$() {
@@ -95,5 +100,7 @@ public class FunctionPanel {
 
     public void setComboboxModel(ComboBoxModel<ComboboxItem> model) {
         cbFunction.setModel(model);
+        if (cbFunction.getItemCount() > 0)
+            cbFunction.setSelectedIndex(cbFunction.getItemCount() - 1);
     }
 }
